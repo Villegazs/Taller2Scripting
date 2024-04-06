d1ef233c4eddde8349be2df83e2c115515112119
Villegazs <alejitov214@hotmail.com>
2024-04-06 16:56:26 -0500

    Ejercicio 2 en visual studio code

---
+++ DelegadoScripting/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace DelegadoScripting
+{
+    internal class Program
+    {
+        public delegate void Tecla(string s);
+
+
+        public static void TeclaW(string s)
+        {
+            Console.WriteLine(s + " W");
+        }
+
+        public static void TeclaA(string s)
+        {
+            Console.WriteLine(s + " A");
+        }
+
+        public static void TeclaS(string s)
+        {
+            Console.WriteLine(s + " S");
+        }
+
+        public static void TeclaD(string s)
+        {
+            Console.WriteLine(s + " D");
+        }
+
+        public static void TeclaF(string s)
+        {
+            Console.WriteLine(s + " F");
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Tecla delegado;
+            delegado = TeclaW;
+            delegado += TeclaA;
+            delegado += TeclaS;
+            delegado += TeclaD;
+            delegado += TeclaF;
+
+            delegado.Invoke("Se oprimio la tecla:");
+
+
+        }
+    }
+}
