b0aaabf706cebe168088252710fcf22e9295d057
Daisu2001 <migueescobar01@gmail.com>
2024-04-11 10:39:42 -0500

    primeras 3

---
+++ funciones.cs
@@ -0,0 +1,21 @@
+//sumar
+
+int Sumar(int a, int b)
+{
+    return a + b;
+}
+
+//restar
+
+int Restar(int a, int b)
+{
+    return a - b;
+}
+
+//multiplicar
+
+int Multiplicar(int a, int b)
+{
+    return a * b;
+}
+
